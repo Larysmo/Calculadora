419c8b990b8b15207819083918e984b7a0c80eac
laryssa <laryss.moraes@gmail.com>
2023-05-25 23:36:10 -0300

    importando repositório calculadora

--- tests/testes.spec.js
+++ tests/testes.spec.js
@@ -1,1 +1,28 @@
-
+ describe('Testando calculadora',()=>{
+     it('Soma de números',()=>{
+     cy.visit('/')
+     cy.get('[value="7"]').click()
+     cy.get('[value="+"]').click()
+     cy.get('[value="6"]').click()
+     cy.get('[value="="]').click()
+     cy.get('#tela').should('have.text','13')
+     })
+     it('Subtração de números',()=>{
+        cy.visit('/')
+        cy.get('[value="7"]').click()
+        cy.get('[value="-"]').click()
+        cy.get('[value="6"]').click()
+        cy.get('[value="="]').click()
+        cy.get('#tela').should('have.text','1')
+        })
+        it('Multiplicação de números',()=>{
+            cy.visit('/')
+            cy.get('[value="7"]').click()
+            cy.get('[value="x"]').click()
+            cy.get('[value="6"]').click()
+            cy.get('[value="="]').click()
+            cy.get('#tela').should('have.text','42')
+            })
+            
+        
+    })
